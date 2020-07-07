79ddd9189fa3ea4c122fa085cae20db500533027
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-07-07 21:38:38 +0900

    手動シグナリングでつながった

--- client/webrtc.js
+++ client/webrtc.js
@@ -4,7 +4,7 @@ const textForSendSdp = document.getElementById("text_for_send_sdp");
 const textToReceiveSdp = document.getElementById("text_for_receive_sdp");
 let localStream = null;
 let peerConnection = null;
-let isOffer = false;
+// let isOffer = false;
 
 async function startVideo() {
   try {
@@ -94,6 +94,9 @@ function prepareNewConnection(isOffer) {
   return peer;
 }
 
+/**
+ * @param {RTCSessionDescription} sessionDescription
+ */
 function sendSdp(sessionDescription) {
   console.log("--- sending sdp ---");
   textForSendSdp.value = sessionDescription.sdp;
@@ -107,5 +110,78 @@ function connect() {
     peerConnection = prepareNewConnection(true);
   } else {
     console.warn("peer connection already exists.");
+  }
+}
+
+async function makeAnswer() {
+  console.log("Sending answer. Creating remote session description...");
+  if (!peerConnection) {
+    console.error("peerConnection does not exist.");
+    return;
+  }
+  try {
+    let answer = await peerConnection.createAnswer();
+    console.log("createAnswer() success in promise");
+
+    await peerConnection.setLocalDescription(answer);
+    console.log("setLocalDescription() success in promise");
+    sendSdp(peerConnection.setLocalDescription);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function onSdpText() {
+  const text = textToReceiveSdp.value;
+  if (peerConnection) {
+    // offer側
+    console.log("Received answer text...");
+    const answer = new RTCSessionDescription({
+      type: "answer",
+      sdp: text,
+    });
+    setAnswer(answer);
+  } else {
+    // answer側
+    console.log("Received offer text...");
+    const offer = new RTCSessionDescription({
+      type: "offer",
+      sdp: text,
+    });
+    setOffer(offer);
+  }
+  textToReceiveSdp.value = "";
+}
+
+/**
+ * @param {RTCSessionDescription} sessionDescription
+ */
+async function setOffer(sessionDescription) {
+  if (peerConnection) {
+    console.error("peerConnection already exists");
+  }
+  peerConnection = prepareNewConnection(false);
+  try {
+    await peerConnection.setRemoteDescription(sessionDescription);
+    console.log("setRemoteDescription(answer) success in promise");
+    makeAnswer();
+  } catch (err) {
+    console.error(`Error at setRemoteDescription(offer): ${err}`);
+  }
+}
+
+/**
+ * @param {RTCSessionDescription} sessionDescription
+ */
+async function setAnswer(sessionDescription) {
+  if (!peerConnection) {
+    console.error("peerConnection does not exist");
+    return;
+  }
+  try {
+    await peerConnection.setRemoteDescription(sessionDescription);
+    console.log("setRemoteDescription(answer) success in promise");
+  } catch (err) {
+    console.error(`Error at setRemoteDescription(answer): ${err}`);
   }
 }
