60f05e9cef0438d5528eb9425ee9125cedff3773
tatsuya4559 <tatsuya.k1029@gmail.com>
2020-07-08 20:58:03 +0900

    シグナリングサーバを実装

--- server/signaling.js
+++ server/signaling.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const WebSocketServer = require("ws").Server;
+const port = 3001;
+const wsServer = new WebSocketServer({ port: port });
+
+wsServer.on("connection", (ws) => {
+  console.log("--- websocket connected ---");
+  ws.on("message", (message) => {
+    wsServer.clients.forEach((client) => {
+      if (isSame(ws, client)) {
+        console.log("skip sender");
+      } else {
+        client.send(message);
+      }
+    });
+  });
+});
+
+function isSame(ws1, ws2) {
+  return ws1 === ws2;
+}
+
+console.log(`websocket server started on port: ${port}`);
+
